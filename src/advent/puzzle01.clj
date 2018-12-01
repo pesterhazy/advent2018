dab7b7cc20e07e5d7a95e64955e2d1d9f52f8864
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-01 19:17:00 +0100

    WIP

--- src/advent/puzzle01.clj
+++ src/advent/puzzle01.clj
@@ -1,22 +1,22 @@
 (ns advent.puzzle01)
 
-(def sample-changes
+(def sample-deltas
   [1 -2 3 1])
 
-(defn read-changes
+(defn read-deltas
   []
   (with-open [f (-> "1/input.txt"
                     clojure.java.io/reader)]
     (->> (line-seq f)
          (mapv #(Long/parseLong %)))))
 
-(defn calc [changes]
+(defn frequency [deltas]
   (let [step (fn [[acc-freq acc-seen] n]
                (let [next-freq (+ acc-freq n)]
                  (if (acc-seen next-freq)
                    (reduced next-freq)
                    [next-freq (conj acc-seen next-freq)])))
-        result (->> changes
+        result (->> deltas
                     cycle
                     (reduce step [0 #{}]))]
     (if (number? result)
@@ -25,7 +25,11 @@
 
 (defn solution
   []
-  (calc (read-changes)))
+  (frequency (read-deltas)))
 
 (defn -main []
   (println (solution)))
+
+(comment
+  (frequency sample-deltas)
+  (frequency (read-deltas)))
