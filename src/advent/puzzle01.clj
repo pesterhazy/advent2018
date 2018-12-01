67ba2d57680cc9f24efd30fd80c8707b7402f998
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-01 18:22:12 +0100

    WIP

--- src/advent/puzzle01.clj
+++ src/advent/puzzle01.clj
@@ -1,4 +1,16 @@
 (ns advent.puzzle01)
 
+(defn lines
+  []
+  (with-open [f (-> "1/input.txt"
+                    clojure.java.io/reader)]
+    (doall (line-seq f))))
+
+(defn solution
+  []
+  (->> (lines)
+       (map #(Long/parseLong %))
+       (reduce + 0)))
+
 (defn -main []
-  (println "puzzle01"))
+  (println (solution)))
