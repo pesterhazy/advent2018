4dff687a485f29ccc579ff9b3450822d730339bf
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-02 09:59:33 +0100

    WIP

--- src/advent/puzzle02.clj
+++ src/advent/puzzle02.clj
@@ -3,6 +3,12 @@
 (def sample-codes
   ["abcdef" "bababc" "abbcde" "abcccd" "aabcdd" "abcdee" "ababab"])
 
+(defn read-codes
+  []
+  (with-open [f (-> "2/input.txt"
+                    clojure.java.io/reader)]
+    (vec (line-seq f))))
+
 (defn checksum
   [codes]
   (let [{twos 2, threes 3} (->> codes
