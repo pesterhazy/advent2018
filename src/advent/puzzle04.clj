2bf8fde0405deb9f92896f67a4835d979790b574
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-04 08:51:19 +0100

    WIP

--- src/advent/puzzle04.clj
+++ src/advent/puzzle04.clj
@@ -1,15 +1,27 @@
 (ns advent.puzzle04)
 
-(def regex #"^\[(....-..-..) (..):(..)\].*$")
+(def regex #"^\[(....-..-..) (..):(..)\] (.*)$")
 
 (defn parse
   [line]
   (let [m (->> (re-matches regex line)
                rest
                ;; (map #(Long/parseLong %))
-               (zipmap [:date :h :m]))]
-    (assoc m :time (+ (* (Long/parseLong (:h m)) 60)
-                      (Long/parseLong (:m m))))))
+               (zipmap [:date :h :m :text]))
+        m* (assoc m
+                  :time (+ (* (Long/parseLong (:h m)) 60) (Long/parseLong (:m m))))]
+    (if-let [g (re-matches #"Guard #(\d+) begins shift" (:text m))]
+      (assoc m*
+             :event :begin
+             :id (Long/parseLong (second g)))
+      (assoc m* :event ({"wakes up" :start, "falls asleep" :end} (:text m))))))
+
+(defn fill-in
+  [xs]
+  (reduce (fn [[acc-xs acc-id] x]
+            (let [id (or (:id x) acc-id)] [(conj acc-xs (assoc x :id id)) id]))
+          [[] nil]
+          xs))
 
 (defn read-input
   []
