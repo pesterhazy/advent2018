a7f3ccca426383ee8d50c628976f4889daf1baaa
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-04 09:14:30 +0100

    WIP

--- src/advent/puzzle04.clj
+++ src/advent/puzzle04.clj
@@ -1,27 +1,57 @@
 (ns advent.puzzle04)
 
-(def regex #"^\[(....-..-..) (..):(..)\] (.*)$")
+(def regex #"^\[(....-..-..) ..:(..)\] (.*)$")
 
 (defn parse
   [line]
   (let [m (->> (re-matches regex line)
                rest
                ;; (map #(Long/parseLong %))
-               (zipmap [:date :h :m :text]))
-        m* (assoc m
-                  :time (+ (* (Long/parseLong (:h m)) 60) (Long/parseLong (:m m))))]
+               (zipmap [:date :min :text]))
+        m* (update m :min #(Long/parseLong %))]
     (if-let [g (re-matches #"Guard #(\d+) begins shift" (:text m))]
       (assoc m*
-             :event :begin
+             :event :set-guard
              :id (Long/parseLong (second g)))
-      (assoc m* :event ({"wakes up" :start, "falls asleep" :end} (:text m))))))
+      (assoc m* :event ({"wakes up" :end, "falls asleep" :start} (:text m))))))
 
 (defn fill-in
-  [xs]
-  (reduce (fn [[acc-xs acc-id] x]
-            (let [id (or (:id x) acc-id)] [(conj acc-xs (assoc x :id id)) id]))
-          [[] nil]
-          xs))
+  [ms]
+  (-> (reduce (fn [[acc-ms acc-id acc-start] m]
+                (let [id (or (:id m) acc-id)
+                      start (if (= :start (:event m)) (:min m) acc-start)]
+                  [(conj acc-ms
+                         (assoc m
+                                :id id
+                                :start start)) id start]))
+              [[] nil nil]
+              ms)
+      first))
+
+(defn events
+  [lines]
+  (->> lines
+       (map parse)
+       fill-in
+       (filter #(= :end (:event %)))
+       (map
+        (fn [m]
+          {:id (:id m), :date (:date m), :start (:start m), :end (:min m)}))))
+
+(defn solution-1
+  [es]
+  (let [id->es (group-by :id es)
+        id+durations (map (fn [[id es]] [id
+                                         (->> es
+                                              (map (fn [e]
+                                                     (- (:end e) (:start e))))
+                                              (apply +))])
+                          id->es)
+        sleepiest-id (->> id+durations
+                          (sort-by (comp - second))
+                          first
+                          first)]
+    sleepiest-id))
 
 (defn read-input
   []
