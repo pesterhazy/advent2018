604ee2866be15d9bbb84e4e3f8824a361b217cda
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-05 08:45:58 +0100

    WIP

---
+++ src/advent/puzzle05.clj
@@ -0,0 +1,35 @@
+(ns advent.puzzle05)
+
+(def sample-input "dabAcCaCBAcCcaDA")
+
+(defn polar?
+  [a b]
+  (and (not= a b) (= (Character/toUpperCase a) (Character/toUpperCase b))))
+
+(defn find-pred
+  [pred xs start-idx reverse?]
+  (->> (if reverse?
+         (range (dec start-idx) -1 -1)
+         (range (inc start-idx) (count xs)))
+       (some (fn [idx] (when (pred (nth xs idx)) idx)))))
+
+(defn scan
+  [xs]
+  (loop [xs (vec xs)
+         idx 0]
+    (cond
+      (>= idx (dec (count xs))) xs
+      (nil? (nth xs idx)) (recur xs (inc idx))
+      :else (let [left (find-pred identity xs idx true)]
+              (if (and left (polar? (nth xs left) (nth xs idx)))
+                (recur (assoc xs
+                              idx nil
+                              left nil)
+                       idx)
+                (let [right (find-pred identity xs idx false)]
+                  (if (and right (polar? (nth xs idx) (nth xs right)))
+                    (recur (assoc xs
+                                  idx nil
+                                  right nil)
+                           idx)
+                    (recur xs (inc idx)))))))))
