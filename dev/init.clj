9d608ac81077770e156e5d39377dab5d4e3c0481
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-08 17:03:22 +0100

    WIP

--- dev/init.clj
+++ dev/init.clj
@@ -1,10 +1,19 @@
+(in-ns 'user)
+
 (require '[nrepl.server])
+(require 'rebel-readline.main)
 (let [port 48888]
   (def server (nrepl.server/start-server :port port))
   (println "nrepl-port:" port))
 
-(require 'rebel-readline.main)
+(def initial-ns (symbol (str "advent." (->> (file-seq (clojure.java.io/file "src")) (map #(.getName %)) (keep #(re-matches #"(puzzle.*)\.clj" %)) (map second) sort last))))
+
+(require initial-ns)
+(in-ns initial-ns)
+
 (rebel-readline.main/-main)
 
+;; done
+
 (shutdown-agents)
-(nrepl.server/stop-server server)
+(nrepl.server/stop-server user/server)
