86918e7d21f2fc27fa80c430bf8de885d41ac287
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-09 15:30:20 +0100

    WIP

--- src/advent/puzzle09.clj
+++ src/advent/puzzle09.clj
@@ -1,13 +1,13 @@
 (ns advent.puzzle09)
 
 (defprotocol IMarbleList
-  (nth-ccw [mlist n] "Returns the marble n places before the current")
   (insert-1 [mlist x]
     "Insert a marble after the 1st element. New marble becomes current")
   (backshift
     [mlist backshift-pos]
     "Removes a marble backshift-pos marble before current.
-Marble after remove marble comes current"))
+Marble after remove marble comes current")
+  (nth-ccw [mlist n] "Returns the marble n places before the current"))
 
 ;; -----------------
 ;; Implementation 1: vector
@@ -33,7 +33,15 @@ Marble after remove marble comes current"))
 ;; -----------------
 ;; Implementation 2
 
-(defn between [a b] (if (< a b) (* 0.5 (+ a b)) (+ a 1.0)))
+;; TODO: what if no space between floats?
+
+(defn between
+  [a b]
+  (let [v (if (< a b) (* 0.5 (+ a b)) (+ a 1.0))]
+    (if (< a v b)
+      v
+      (throw (ex-info "Floating-point precision exhausted"
+                      {:a a, :b b, :v v})))))
 
 (defn find-pos
   [pos ps]
@@ -49,11 +57,11 @@ Marble after remove marble comes current"))
 
 (defrecord FastMarbleList [m current-k]
   IMarbleList
-  (insert-1 [_ x]
-    (let [new-k (find-pos current-k (keys m))]
-      (->FastMarbleList (assoc m new-k x) new-k)))
-  (backshift [_ backshift-pos] m)
-  (nth-ccw [_ n] nil))
+    (insert-1 [_ x]
+      (let [new-k (find-pos current-k (keys m))]
+        (->FastMarbleList (assoc m new-k x) new-k)))
+    (backshift [_ backshift-pos] m)
+    (nth-ccw [_ n] nil))
 
 (defn empty-fast-mlist [] (->FastMarbleList (sorted-map) nil))
 
