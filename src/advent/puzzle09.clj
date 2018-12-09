f8ff75da6eabc80a4cf00a4382a5ef1d3766317d
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-09 13:25:41 +0100

    WIP

--- src/advent/puzzle09.clj
+++ src/advent/puzzle09.clj
@@ -1,7 +1,30 @@
 (ns advent.puzzle09)
 
-(defn turn [xs x]
-  (into [x] (->> xs
-                 cycle
-                 (drop 2)
-                 (take (count xs)))))
+(defn nth-ccw [xs n] (nth xs (- (count xs) n)))
+
+(defn game
+  [{:keys [n-players backshift bingo]} marbles]
+  (let [turn (fn [[xs score] x]
+               (if (zero? (mod x bingo))
+                 (let [player (inc (mod (dec x) n-players))]
+                   [(->> xs
+                         cycle
+                         (drop (- (count xs) (dec backshift)))
+                         (take (dec (count xs)))
+                         vec)
+                    (update score
+                            player
+                            (fn [n] (+ (or n 0) x (nth-ccw xs backshift))))])
+                 [(into [x]
+                        (->> xs
+                             cycle
+                             (drop 2)
+                             (take (count xs)))) score]))]
+    (second (reduce turn [[1] nil] marbles))))
+
+(defn solution-1
+  []
+  (->> (range 1 (inc 70784))
+       (game {:backshift 7, :bingo 23, :n-players 452})
+       vals
+       (apply max)))
