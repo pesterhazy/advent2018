15b2f06a6c9e6e1025f1127d6999a2fc888389cf
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-09 14:03:57 +0100

    WIP

--- src/advent/puzzle09.clj
+++ src/advent/puzzle09.clj
@@ -1,41 +1,48 @@
 (ns advent.puzzle09)
 
-(defn nth-ccw [xs n] (nth xs (- (count xs) n)))
+(defn empty-mlist [] [])
+
+(defn nth-ccw
+  "Returns the marble n places before the current"
+  [mlist n]
+  (nth mlist (- (count mlist) n)))
 
 (defn insert-1
-  "Insert a marble before the 2nd element"
-  [xs x]
+  "Insert a marble before the 2nd element. New marble becomes current"
+  [mlist x]
   (into [x]
-        (->> xs
+        (->> mlist
              cycle
              (drop 2)
-             (take (count xs)))))
+             (take (count mlist)))))
 
 (defn backshift
-  [xs backshift-pos]
-  (->> xs
+  "Removes a marble backshift-pos marble before current. Marble after remove
+  marble comes current"
+  [mlist backshift-pos]
+  (->> mlist
        cycle
-       (drop (- (count xs) (dec backshift-pos)))
-       (take (dec (count xs)))
+       (drop (- (count mlist) (dec backshift-pos)))
+       (take (dec (count mlist)))
        vec))
 
 (defn simulate
   "Takes a sequence of marbles, returns score"
   [{:keys [n-players backshift-pos bingo]} marbles]
-  (let [turn
-        (fn [[xs score] x]
-          (if (zero? (mod x bingo))
-            (let [player (inc (mod (dec x) n-players))]
-              [(backshift xs backshift-pos)
-               (update score
-                       player
-                       (fn [n] (+ (or n 0) x (nth-ccw xs backshift-pos))))])
-            [(insert-1 xs x) score]))]
-    (second (reduce turn [[1] nil] marbles))))
+  (let [turn (fn [[mlist score] x]
+               (if (and (pos? x) (zero? (mod x bingo)))
+                 (let [player (inc (mod (dec x) n-players))]
+                   [(backshift mlist backshift-pos)
+                    (update score
+                            player
+                            (fn [n]
+                              (+ (or n 0) x (nth-ccw mlist backshift-pos))))])
+                 [(insert-1 mlist x) score]))]
+    (second (reduce turn [(empty-mlist) nil] marbles))))
 
 (defn winner
   [n-players n-marbles]
-  (->> (range 1 (inc n-marbles))
+  (->> (range 0 (inc n-marbles))
        (simulate {:backshift-pos 7, :bingo 23, :n-players n-players})
        vals
        (apply max)))
