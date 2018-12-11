d4ac5892e835005351c5e2052e249dead16d6484
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-11 09:05:38 +0100

    WIP

--- src/advent/puzzle11.clj
+++ src/advent/puzzle11.clj
@@ -1,5 +1,8 @@
 (ns advent.puzzle11)
 
+(set! *unchecked-math* :warn-on-boxed)
+(set! *warn-on-reflection* true)
+
 (def grid-sn 4151)
 
 ;; Find the fuel cell's rack ID, which is its X coordinate plus 10.
@@ -9,21 +12,21 @@
 ;; Keep only the hundreds digit of the power level (so 12345 becomes 3; numbers with no hundreds digit become 0).
 ;; Subtract 5 from the power level.
 
-(defn hun [n]
-  (int (/ (mod n 1000) 100)))
+(defn hun ^long [^long n]
+  (int (/ ^long (mod n 1000) 100)))
 
-(defn ->level [grid-sn x y]
-  (- (hun (* (+ x 10) (+ (* y(+ x 10)) grid-sn))) 5))
+(defn ->level ^long [^long grid-sn ^long x ^long y]
+  (- (hun (* (+ x 10) (+ (* y (+ x 10)) grid-sn))) 5))
 
 (def width 300)
 (def height 300)
 
 (defn gen-blocks []
-  (->> (for [y (range (- height 3))
-             x (range (- width 3))]
+  (->> (for [y (range (- ^long height 3))
+             x (range (- ^long width 3))]
          [[x y]
-          (for [y* (range y (+ y 3))
-                x* (range x (+ x 3))]
+          (for [y* (range y (+ ^long y 3))
+                x* (range x (+ ^long x 3))]
             [x* y*])])
        (into {})))
 
@@ -37,7 +40,9 @@
 (defn solution-1 [blocks]
   (->> blocks
        (map (fn [[k pairs]]
-              (->> pairs
-                   (map (fn [pair] (apply ->level grid-sn pair)))
-                   (apply +))))
-       (apply max)))
+              [k
+               (->> pairs
+                    (map (fn [pair] (apply ->level grid-sn pair)))
+                    (apply +))]))
+       (apply max-key second)
+       first))
