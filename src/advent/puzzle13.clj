a4e63ebbf7dc2300abfd1872b93459917ae56bfd
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-13 16:23:46 +0100

    WIP

--- src/advent/puzzle13.clj
+++ src/advent/puzzle13.clj
@@ -9,10 +9,28 @@
 (defn point-in [graph x y]
   (nth (nth graph y) x))
 
-(def cart-symbol? #{\^ \> \v \<})
+(def cart->line {\^ \|
+                 \> \-
+                 \v \-
+                 \< \|})
 
 (defn find-carts [graph]
-  (for [[y line] (map-indexed vector graph),
-        [x point] (map-indexed vector line)
-        :when (cart-symbol? point)]
-    [x y point]))
+  (->> (for [[y line] (map-indexed vector graph),
+             [x point] (map-indexed vector line)
+             :when (cart->line point)]
+         [[x y] point])
+       (into (sorted-map))))
+
+(defn tick [graph carts]
+  carts)
+
+(defn print-graph [graph carts]
+  (doseq [[y line] (map-indexed vector graph)]
+    (println (apply str (for [[x point] (map-indexed vector line)]
+                          (or (carts [x y])
+                              (cart->line point point)))))))
+
+(defn solution-1 []
+  (let [graph (read-sample)
+        carts (find-carts graph)]
+    (print-graph (tick graph carts))))
