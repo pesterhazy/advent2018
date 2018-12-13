6bc3eeef4670f1eae722abf84737d059cba8a390
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-13 11:16:01 +0100

    WIP

--- src/advent/puzzle12.clj
+++ src/advent/puzzle12.clj
@@ -2,38 +2,43 @@
   (:import java.util.BitSet)
   (:require [clojure.string :as str]))
 
+(def padding 3)
+
 (defn read-sample
   []
   (with-open [f (-> "12/sample.txt"
                     clojure.java.io/reader)]
     (vec (line-seq f))))
 
-(defn s->bitset [s]
+(defn s->bitset
+  [s]
   (let [bs (BitSet. (count s))]
-    (doseq [[idx c] (map-indexed vector s)]
-      (when (= \# c)
-        (.set bs idx)))
+    (doseq [[idx c] (map-indexed vector s)] (when (= \# c) (.set bs idx)))
     bs))
 
-(defn bitset->s [s]
+(defn bitset->s
+  [s]
   (->> (range (.length s))
-       (map (fn [idx]
-              (if (.get s idx) \# \.)))
+       (map (fn [idx] (if (.get s idx) \# \.)))
        (str/join)))
 
-(def regex
-  #"initial state: (.+)")
+(def regex #"initial state: (.+)")
 
-(defn parse-header [line]
-  (-> (re-matches regex line)
-      second
-      ->bitset))
+(defn parse-header
+  [line]
+  (->> (re-matches regex line)
+       second
+       (str (apply str (repeat padding \.)))
+       ->bitset))
 
 (def regex2 #"^([#.]+) => ([#.])$")
 
-(defn parse-body-line [line]
+(defn parse-body-line
+  [line]
   (let [[_ k v] (re-matches regex2 line)]
-    [(-> k ->bitset .toLongArray first) (= \# v)]))
+    [(-> k
+         ->bitset
+         .toLongArray
+         first) (= \# v)]))
 
-(defn parse [[fst _ & body]]
-  [(parse-header fst) (mapv parse-body-line body)])
+(defn parse [[fst _ & body]] [(parse-header fst) (mapv parse-body-line body)])
