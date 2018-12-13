0bc262ac4913345de3c718e2b4e3c370a1cd4086
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-13 11:51:36 +0100

    WIP

--- src/advent/puzzle12.clj
+++ src/advent/puzzle12.clj
@@ -2,7 +2,8 @@
   (:import java.util.BitSet)
   (:require [clojure.string :as str]))
 
-(def padding 3)
+(def padding 2)
+(def blength 5)
 
 (defn read-sample
   []
@@ -17,10 +18,15 @@
     bs))
 
 (defn bitset->s
-  [s]
-  (->> (range (.length s))
-       (map (fn [idx] (if (.get s idx) \# \.)))
-       (str/join)))
+  ([s]
+   (->> (range (.length s))
+        (map (fn [idx] (if (.get s idx) \# \.)))
+        (str/join)))
+  ([s length]
+   (->> (range length)
+        (map (fn [idx] (if (.get s idx) \# \.)))
+        (str/join)))
+  )
 
 (def regex #"initial state: (.+)")
 
@@ -28,20 +34,41 @@
   [line]
   (let [s (->> (re-matches regex line)
                second)]
-    {:length (count s)
-     :initial-state (s->bitset (str (apply str (repeat padding \.)) s))}))
+    {:length (count s), :initial-state (s->bitset s)}))
 
 (def regex2 #"^([#.]+) => ([#.])$")
 
 (defn parse-body-line
   [line]
   (let [[_ k v] (re-matches regex2 line)]
-    (prn k v)
     [(-> k
          s->bitset
          .toLongArray
          first) (= "#" v)]))
 
-(defn parse [[fst _ & body]]
+(defn parse
+  [[fst _ & body]]
   (-> (parse-header fst)
-      (assoc :lookup (->> body (map parse-body-line) (into {})))))
+      (assoc :lookup (->> body
+                          (map parse-body-line)
+                          (into {})))))
+
+(defn sub-bitset [bs from to]
+  (let [new-bs (BitSet.)]
+    (doseq [[new-idx old-idx] (map-indexed vector (range from to))]
+      (.set new-bs new-idx (if (neg? old-idx)
+                             false
+                             (.get bs old-idx))))
+    new-bs))
+
+(defn next-gen
+  [ctx bs]
+  (doseq [idx (range (- padding) (:length ctx))]
+    (let [v (if (neg? idx) false (.get bs idx))]
+      (println (format "%3d" idx) (bitset->s (sub-bitset bs
+                                                         (- idx padding)
+                                                         (+ idx padding 1))
+                                             blength))))
+  bs)
+
+(defn generations [ctx] (iterate #(next-gen ctx %) (:initial-state ctx)))
