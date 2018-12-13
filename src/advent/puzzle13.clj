3be1a6487b83bab4c0c96d4641486718e7b745e9
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-13 17:06:13 +0100

    WIP

--- src/advent/puzzle13.clj
+++ src/advent/puzzle13.clj
@@ -14,35 +14,48 @@
                  \v \-
                  \< \|})
 
+(def directions [\^ \> \v \<])
+
 (defn find-carts [graph]
   (->> (for [[y line] (map-indexed vector graph),
              [x point] (map-indexed vector line)
              :when (cart->line point)]
-         [[x y] point])
+         [[x y] {:sym point
+                 :n-turns 0}])
        (into (sorted-map))))
 
-(defn transform-cart [graph [[x y] cart-symbol]]
+(defn rotate [sym nturns]
+  (case (mod nturns 3)
+    0 (nth directions (mod (dec (.indexOf directions sym)) 4))
+    1 sym
+    2 (nth directions (mod (inc (.indexOf directions sym)) 4))))
+
+(defn transform-cart [graph [[x y] {:keys [n-turns sym] :as cart}]]
   (let [[new-x new-y]
-        (case cart-symbol
+        (case sym
           \^ [x (dec y)]
           \v [x (inc y)]
           \< [(dec x) y]
           \> [(inc x) y])
         point (point-in graph new-x new-y)
-        ky [cart-symbol point]
-        new-symbol (case ky
-                     [\> \\] \v
-                     [\> \/] \^
-                     [\< \\] \^
-                     [\< \/] \v
-                     [\^ \\] \<
-                     [\^ \/] \>
-                     [\v \\] \>
-                     [\v \/] \<
-                     (do
-                       (assert (#{\- \|} point) (str "Unexpected: " ky))
-                       cart-symbol))]
-    [[x y] new-symbol]))
+        ky [sym point]
+        new-cart (case point
+                   (\| \-)
+                   cart
+                   \+
+                   {:n-turns (inc n-turns)
+                    :sym (rotate sym n-turns)}
+                   {:n-turns n-turns
+                    :sym (case ky
+                           [\> \\] \v
+                           [\> \/] \^
+                           [\< \\] \^
+                           [\< \/] \v
+                           [\^ \\] \<
+                           [\^ \/] \>
+                           [\v \\] \>
+                           [\v \/] \<)})]
+    [[new-x new-y] new-cart]))
 
 (defn tick [graph carts]
   (->> carts
@@ -52,10 +65,12 @@
 (defn print-graph [graph carts]
   (doseq [[y line] (map-indexed vector graph)]
     (println (apply str (for [[x point] (map-indexed vector line)]
-                          (or (carts [x y])
+                          (or (some-> (carts [x y]) :sym)
                               (cart->line point point)))))))
 
 (defn solution-1 []
   (let [graph (read-sample)
-        carts (find-carts graph)]
-    (print-graph graph (tick graph carts))))
+        generations (->> (iterate (partial tick graph) (find-carts graph))
+                         (take 5))]
+    (doseq [generation generations]
+      (print-graph graph generation))))
