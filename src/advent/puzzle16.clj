effad77caa88f30efc7498b2f9b9b0aa858b394e
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-16 15:49:44 +0100

    WIP

--- src/advent/puzzle16.clj
+++ src/advent/puzzle16.clj
@@ -7,12 +7,14 @@
                     clojure.java.io/reader)]
     (vec (line-seq f))))
 
-(def regex #"(?x)
+(def regex
+  #"(?x)
 Before:\s+\[(\d+),\s+(\d+),\s+(\d+),\s+(\d+)\]\n
 (\d+)\s+(\d+)\s+(\d+)\s+(\d+)\n
 After:\s+\[(\d+),\s+(\d+),\s+(\d+),\s+(\d+)\]")
 
-(defn match [lines]
+(defn match
+  [lines]
   (let [matches (re-matches regex (str/join "\n" lines))]
     (assert matches)
     (-> (->> (rest matches)
@@ -25,62 +27,40 @@ After:\s+\[(\d+),\s+(\d+),\s+(\d+),\s+(\d+)\]")
 (def t-pattern (match (read-sample)))
 
 (def opcodes
-  [:addr
-   :addi
-   :mulr
-   :muli
-   :banr
-   :bani
-   :borr
-   :bori
-   :setr
-   :seti
-   :gtir
-   :gtri
-   :gtrr
-   :eqir
-   :eqri
-   :eqrr])
+  [:addr :addi :mulr :muli :banr :bani :borr :bori :setr :seti :gtir :gtri :gtrr
+   :eqir :eqri :eqrr])
 
-(defn gt [a b]
-  (if (> a b) 1 0))
+(defn gt [a b] (if (> a b) 1 0))
 
-(defn eq [a b]
-  (if (= a b) 1 0))
+(defn eq [a b] (if (= a b) 1 0))
 
-(defn apply-op [regs {:keys [opcode a b c]}]
+(defn apply-op
+  [regs {:keys [opcode a b c]}]
   (case opcode
     :addr (assoc regs c (+ (regs a) (regs b)))
     :addi (assoc regs c (+ (regs a) b))
-
     :mulr (assoc regs c (* (regs a) (regs b)))
     :muli (assoc regs c (* (regs a) b))
-
     :banr (assoc regs c (bit-and (regs a) (regs b)))
     :bani (assoc regs c (bit-and (regs a) b))
-
     :borr (assoc regs c (bit-or (regs a) (regs b)))
     :bori (assoc regs c (bit-or (regs a) b))
-
     :setr (assoc regs c (regs a))
     :seti (assoc regs c a)
-
     :gtir (assoc regs c (gt a (regs b)))
     :gtri (assoc regs c (gt (regs a) b))
     :gtrr (assoc regs c (gt (regs a) (regs b)))
-
     :eqir (assoc regs c (eq a (regs b)))
     :eqri (assoc regs c (eq (regs a) b))
-    :eqrr (assoc regs c (eq (regs a) (regs b)))
-    ))
+    :eqrr (assoc regs c (eq (regs a) (regs b)))))
 
-(defn try-pattern-one [pattern substitute]
+(defn try-pattern-one
+  [pattern substitute]
   (= (apply-op (:before pattern) (assoc (:op pattern) :opcode substitute))
      (:after pattern)))
 
-(defn try-pattern [pattern]
+(defn try-pattern
+  [pattern]
   (->> opcodes
-       (keep (fn [opcode]
-               (when (try-pattern-one pattern opcode)
-                 opcode)))
+       (keep (fn [opcode] (when (try-pattern-one pattern opcode) opcode)))
        set))
