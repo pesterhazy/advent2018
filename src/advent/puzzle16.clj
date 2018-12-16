8b734c025eaec0356ac24f475a9fdaddd6476072
Paulus Esterhazy <pesterhazy@gmail.com>
2018-12-16 15:11:48 +0100

    WIP

--- src/advent/puzzle16.clj
+++ src/advent/puzzle16.clj
@@ -7,8 +7,6 @@
                     clojure.java.io/reader)]
     (vec (line-seq f))))
 
-(def t-data (read-sample))
-
 (def regex #"(?x)
 Before:\s+\[(\d+),\s+(\d+),\s+(\d+),\s+(\d+)\]\n
 (\d+)\s+(\d+)\s+(\d+)\s+(\d+)\n
@@ -17,7 +15,19 @@ After:\s+\[(\d+),\s+(\d+),\s+(\d+),\s+(\d+)\]")
 (defn match [lines]
   (let [matches (re-matches regex (str/join "\n" lines))]
     (assert matches)
-    (->> (rest matches)
-         (partition 4)
-         (map (partial mapv #(Long/parseLong %)))
-         (zipmap [:before :op :after]))))
+    (-> (->> (rest matches)
+             (partition 4)
+             (map (partial mapv #(Long/parseLong %)))
+             (zipmap [:before :op :after]))
+        (update :op (partial zipmap [:opcode :a :b :c])))))
+
+(def t-data (read-sample))
+(def t-pattern (match (read-sample)))
+
+(defn apply-op [regs {:keys [opcode a b c]}]
+  (case opcode
+    :mulr (assoc regs c (* (regs a) (regs b)))))
+
+(defn try-pattern [pattern substitute]
+  (= (apply-op (:before pattern) (assoc (:op pattern) :opcode substitute))
+     (:after pattern)))
